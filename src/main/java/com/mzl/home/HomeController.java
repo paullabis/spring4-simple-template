c5a90323d2ebaabf9131d0ecf5e89a96176e9606
paullabis <paul.labis@practiceinsight.io>
2016-09-09 01:33:57 +0800

    default data init

--- src/main/java/com/mzl/home/HomeController.java
+++ src/main/java/com/mzl/home/HomeController.java
@@ -1,15 +1,21 @@
 package com.mzl.home;
 
+import com.mzl.service.CategoryService;
+
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
-	
+  @Autowired
+  private CategoryService categoryService;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Principal principal) {
+    categoryService.getCategoryList(); // default data init
 		return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
 	}
 }
